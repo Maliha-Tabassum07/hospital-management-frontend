40dd36c559d4821274f061bbd97593f4abf28d7a
Maliha Tabassum <maliha.tabassum@bjitacademy.com>
2023-11-20 07:03:38 +0600

    Landing page

--- hospital-management-system-frontend/src/components/Header.jsx
+++ hospital-management-system-frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ import "./Header.css"; // Make sure to create a corresponding CSS file
 function Header() {
   return (
     <header className="header">
-      <h1>United Hospital</h1>
+      <h1>HMS</h1>
       <p>
         Hotline: 10666 | Emergency: +880 1914 001234 | Appointment Hotline: 02
         22 22 62 466
